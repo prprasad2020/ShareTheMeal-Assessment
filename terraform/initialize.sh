4f8d95558163284e1c611a1a7f918ffcfea25327
Priyankar Prasad <priyankar9805@gmail.com>
2022-02-13 11:57:04 +0530

    Fixed Permission issues
    Fixed root permissions when running init script and Ansible script

--- terraform/initialize.sh
+++ terraform/initialize.sh
@@ -2,4 +2,4 @@
 sudo amazon-linux-extras install ansible2 -y
 cd /tmp
 wget https://raw.githubusercontent.com/prprasad2020/ShareTheMeal-Assessment/main/ansible/bootstrap-ansible.yml
-ansible-playbook /tmp/bootstrap-ansible.yml
+sudo ansible-playbook /tmp/bootstrap-ansible.yml
